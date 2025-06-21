83d9ec1e3db04e52ab3b7dd4ad6923ec1e59b2ad
Dambadarjaa <57674891+Antaqor@users.noreply.github.com>
2025-06-21 19:27:26 +0800

    Refine dark theme and header design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ module.exports = {
     extend: {
       colors: {
         brandCyan: '#00FFFC',
-        supportBorder: '#F6F3E1',
+        supportBorder: '#2D2D2D',
+        primary: '#212121',
+        secondary: '#181818',
+        inputBg: '#1E1E1E',
+        inputText: '#AFAFAF',
       },
       fontFamily: {
         sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'], // Minimalistic and clean font stack
